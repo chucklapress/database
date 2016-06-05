ba06475c4f1af64fe89cf6954b67275c6b7d7650
chucklapress <charleslapress@gmail.com>
2016-06-05 08:41:58 -0400

    changes

--- inout_footballstat.py
+++ inout_footballstat.py
@@ -6,7 +6,7 @@ cursor = connection.cursor()
 cursor.execute("DROP TABLE IF EXISTS quarterback_stat")
 table_create_command = """CREATE TABLE quarterback_stat (
   full_name varchar(30),
-  team varchar(30),
+  team_name varchar(30),
   complete numeric(3),
   attempt numeric(4),
   att_pct numeric(3)
@@ -14,7 +14,7 @@ table_create_command = """CREATE TABLE quarterback_stat (
 
 cursor.execute(table_create_command)
 full_name ='Kirk Cousins'
-team ='WAS'
+team_name ='WAS'
 complete =29
 attempt =46
 att_pct =63
@@ -27,7 +27,7 @@ cursor.execute("INSERT INTO quarterback_stat VALUES('Peyton Manning', 'DEN', 51,
 cursor.execute("INSERT INTO quarterback_stat VALUES('Russell Wilson', 'SEA', 44, 74, 60);")
 cursor.execute("INSERT INTO quarterback_stat VALUES('Aaron Rodgers', 'GB', 45, 80, 56);")
 cursor.execute("INSERT INTO quarterback_stat VALUES('Alex Smith', 'KC', 46, 72, 64);")
-cursor.execute("INSERT INTO quarterback_stat VALUES (%s, %s, %s, %s, %s);" ,(full_name, team, complete, attempt, att_pct))
+cursor.execute("INSERT INTO quarterback_stat VALUES (%s, %s, %s, %s, %s);" ,(full_name, team_name, complete, attempt, att_pct))
 connection.commit()
 
 cursor.close()
